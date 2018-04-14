6b59de8ab98ef7c215d465ff6e9f9756c4a7ac2a
MorganRush <matvey159@gmail.com>
2018-04-14 23:04:44 +0300

    sequelize

--- server/models/index.js
+++ server/models/index.js
@@ -3,19 +3,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 const configJS = require('../config/config');
-
-// let sequelize;
-// if (config.use_env_variable) {
-//     sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else {
-//     sequelize = new Sequelize(
-//         config.database, config.username, config.password, config
-//     );
-// }
 
 const sequelize = new Sequelize(
     configJS.db.database,
